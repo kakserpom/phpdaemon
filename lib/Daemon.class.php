ffde06cfd8e290ab735abe5a0e032ef6f394d11c
Travis Bickle <kak.serpom.po.yaitsam@gmail.com>
2010-05-27 17:22:10 +0400

    Added method Daemon::getMIME()

--- lib/Daemon.class.php
+++ lib/Daemon.class.php
@@ -117,6 +117,79 @@ class Daemon
   else {Daemon::log('Unexcepted output (len. '.strlen($s).'): \''.$s.'\'');}
   return '';
  }
+ /* @method getMIME()
+    @param string - Path
+    @description Returns MIME type of the given file.
+    @return string - MIME type.
+ */
+ public static function getMIME($path)
+ {
+  static $types = array(
+            'txt' => 'text/plain',
+            'htm' => 'text/html',
+            'html' => 'text/html',
+            'php' => 'text/html',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            'swf' => 'application/x-shockwave-flash',
+            'flv' => 'video/x-flv',
+
+            // images
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
+
+            // archives
+            'zip' => 'application/zip',
+            'rar' => 'application/x-rar-compressed',
+            'exe' => 'application/x-msdownload',
+            'msi' => 'application/x-msdownload',
+            'cab' => 'application/vnd.ms-cab-compressed',
+
+            // audio/video
+            'mp3' => 'audio/mpeg',
+            'qt' => 'video/quicktime',
+            'mov' => 'video/quicktime',
+
+            // adobe
+            'pdf' => 'application/pdf',
+            'psd' => 'image/vnd.adobe.photoshop',
+            'ai' => 'application/postscript',
+            'eps' => 'application/postscript',
+            'ps' => 'application/postscript',
+
+            // ms office
+            'doc' => 'application/msword',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'ppt' => 'application/vnd.ms-powerpoint',
+
+            // open office
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+  );
+  $ext = strtolower(pathinfo($path,PATHINFO_EXTENSION));
+  if (isset($types[$ext])) {return $types[$ext];}
+  elseif (function_exists('finfo_open'))
+  {
+   if (!is_readable($path)) {return 'application/octet-stream';}
+   $finfo = finfo_open(FILEINFO_MIME);
+   $mimetype = finfo_file($finfo, $path);
+   finfo_close($finfo);
+   return $mimetype;
+  }
+  else {return 'application/octet-stream';}
+ }
  /* @method header
     @param string
     @description Static wrapper of Request->header().
