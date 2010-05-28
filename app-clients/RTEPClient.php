2b715f84fcac37dea79f7d2fcf559807467ea81e
Travis Bickle <kak.serpom.po.yaitsam@gmail.com>
2010-05-28 19:07:04 +0400

    Removed old code from RTEPClient.php

--- app-clients/RTEPClient.php
+++ app-clients/RTEPClient.php
@@ -13,8 +13,6 @@ class RTEPClient extends AppInstance
    'mod'.$this->modname.'listen' => 'tcpstream://127.0.0.1:844',
    'mod'.$this->modname.'enable' => 0,
   ));
-  if (!isset(Daemon::$settings[$k = 'mod'.$this->modname.'addr'])) {Daemon::$settings[$k] = 'tcpstream://127.0.0.1:844';}
-  if (!isset(Daemon::$settings[$k = 'mod'.$this->modname.'enable'])) {Daemon::$settings[$k] = 0;}
   if (Daemon::$settings['mod'.$this->modname.'enable'])
   {
    Daemon::log(__CLASS__.' up.');
