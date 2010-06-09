1a08c041e2e7b916b86c1c3afcf95660bbc13e9d
Travis Bickle <kak.serpom.po.yaitsam@gmail.com>
2010-06-09 16:55:10 +0400

    Minor changes in default conf/phpdaemon.conf.php

--- conf/phpdaemon.conf.php
+++ conf/phpdaemon.conf.php
@@ -2,12 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors','1');
 return array(
- 'mod-fastcgi-listen' => 'tcp://127.0.0.1,unix:/tmp/phpfastcgi.sock',
  'mod-fastcgi-enable' => 1,
  //'mod-debugconsole-enable' => 1,
  //'mod-telnethoneypot-enable' => 1,
  //'mod-flashpolicy-enable' => 1,
- //'mod-websocketserver-enable' => 1,
+ 'mod-websocketserver-enable' => 1,
  'max-requests' => 1000,
  'max-idle' => 0,
  'min-spare-workers' => 5,
@@ -15,8 +14,8 @@ return array(
  'start-workers' => 20,
  'max-workers' => 50,
  'min-workers' => 20,
- 'user' => 'web',
- 'group' => 'web',
+ 'user' => '',
+ 'group' => '',
  'path' => dirname(__FILE__).'/appResolver.php',
  //'mpm' => function() {},
  'max-concurrent-requests-per-worker' => 1000,
