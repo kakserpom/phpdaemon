b468df862c5134f9f14709a3bf8169444a87580a
Travis Bickle <kak.serpom.po.yaitsam@gmail.com>
2010-07-08 00:32:54 +0400

    PostgreSQLClient: fixed authentication issues.

--- app-clients/PostgreSQLClient.php
+++ app-clients/PostgreSQLClient.php
@@ -311,13 +311,13 @@ class PostgreSQLClientSession extends SocketSession
    elseif ($authType === 4) // Crypt
    {
     $salt = binarySubstr($packet,4,2);
-    $this->sendPacket('p',crypt($this->password,$this->user)); // Password Message
+    $this->sendPacket('p',crypt($this->password,$salt)); // Password Message
     $this->cstate = 2; // Auth. packet sent
    }
    elseif ($authType === 5) // MD5
    {
     $salt = binarySubstr($packet,4,4);
-    $this->sendPacket('p','md5'.md5(md5($this->password,$this->user).$salt)); // Password Message
+    $this->sendPacket('p',md5($this->password.$salt,TRUE)); // Password Message
     $this->cstate = 2; // Auth. packet sent
    }
    elseif ($authType === 6) // SCM
