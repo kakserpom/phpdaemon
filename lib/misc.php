7a960f6598e47fa768e91ca405ba14647e7a7207
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-09-22 22:40:20 +0400

    beautification

--- lib/misc.php
+++ lib/misc.php
@@ -1,26 +1,41 @@
 <?php
-if (ini_get('mbstring.func_overload') >= 2)
-{
- function binarySubstr($s,$p,$l = 0xFFFFFFF)
- {
-  return substr($s,$p,$l,'ASCII');
- }
+
+if (ini_get('mbstring.func_overload') >= 2) {
+	function binarySubstr($s, $p, $l = 0xFFFFFFF) {
+		return substr($s, $p, $l, 'ASCII');
+	}
+} else {
+	function binarySubstr($s, $p, $l = NULL) {
+		if ($l === NULL) {
+			$ret = substr($s, $p);
+		} else {
+			$ret = substr($s, $p, $l);
+		}
+
+		if ($ret === FALSE) {
+			$ret = '';
+		}
+
+		return $ret;
+	}
 }
-else
-{
- function binarySubstr($s,$p,$l = NULL)
- {
-  if ($l === NULL) {$ret = substr($s,$p);}
-  else {$ret = substr($s,$p,$l);}
-  if ($ret === FALSE) {$ret = '';}
-  return $ret;
- }
-}
-class DestructableLambda
-{
- public $id;
- public $hits = 0;
- public function __construct($id) {$this->id = (int) binarySubstr($id,8);}
- public function __invoke() {return call_user_func_array("\x00lambda_".$this->id,func_get_args());}
- public function __destruct() {runkit_function_remove("\x00lambda_".$this->id);}
+
+class DestructableLambda {
+	public $id;
+	public $hits = 0;
+	
+	public function __construct($id) {
+		$this->id = (int) binarySubstr($id,8);
+	}
+
+	public function __invoke() {
+		return call_user_func_array(
+			"\x00lambda_" . $this->id, 
+			func_get_args()
+		);
+	}
+
+	public function __destruct() {
+		runkit_function_remove("\x00lambda_" . $this->id);
+	}
 }
--- lib/threadCollection.class.php
+++ lib/threadCollection.class.php
@@ -1,76 +1,88 @@
 <?php
-class ThreadCollection
-{
- public $threads = array();
- public $waitstatus;
- public $spawncounter = 0;
- /* @method push
-    @description Pushes certain thread to the collection.
-    @param object Thread to push.
-    @return void
- */
- public function push($thread)
- {
-  ++$this->spawncounter;
-  $thread->spawnid = $this->spawncounter;
-  $this->threads[$thread->spawnid] = $thread;
- }
- /* @method start
-    @description Starts the collected threads.
-    @return void
- */
- public function start()
- {
-  foreach ($this->threads as $thread)
-  {
-   $thread->start();
-  }
- }
- /* @method stop
-    @description Stops the collected threads.
-    @return void
- */
- public function stop($kill = FALSE)
- {
-  foreach ($this->threads as $thread)
-  {
-   $thread->stop($kill);
-  }
- }
- /* @method getNumber
-    @description Returns a number of collected threads.
-    @return integer Number.
- */
- public function getNumber()
- {
-  return sizeof($this->threads);
- }
- /* @method removeTerminated
-    @description Removes terminated threads from the collection.
-    @param boolean Whether to check the threads using signal.
-    @return integer Number of removed threads.
- */
- public function removeTerminated($check = FALSE)
- {
-  $n = 0;
-  foreach ($this->threads as $k => &$t)
-  {
-   if ($t->terminated) {unset($this->threads[$k]);}
-   elseif ($check && (!$t->signal(SIGTTIN))) {unset($this->threads[$k]);}
-   else {++$n;}
-  }
-  return $n;
- }
- /* @method signal
-    @description Sends a signal to threads.
-    @param integer Signal's number.
-    @return void
- */
- public function signal($sig)
- {
-  foreach ($this->threads as $thread)
-  {
-   $thread->signal($sig);
-  }
- }
+
+class ThreadCollection {
+	public $threads = array();
+	public $waitstatus;
+	public $spawncounter = 0;
+
+	/**
+	 * @method push
+	 * @description Pushes certain thread to the collection.
+	 * @param object Thread to push.
+	 * @return void
+	 */
+	public function push($thread) {
+		++$this->spawncounter;
+		$thread->spawnid = $this->spawncounter;
+		$this->threads[$thread->spawnid] = $thread;
+	}
+
+	/**
+	 * @method start
+	 * @description Starts the collected threads.
+	 * @return void
+	 */
+	public function start() {
+		foreach ($this->threads as $thread) {
+			$thread->start();
+		}
+	}
+
+	/**
+	 * @method stop
+	 * @description Stops the collected threads.
+	 * @return void
+	 */
+	public function stop($kill = FALSE) {
+		foreach ($this->threads as $thread) {
+			$thread->stop($kill);
+		}
+	}
+
+	/**
+	 * @method getNumber
+	 * @description Returns a number of collected threads.
+	 * @return integer Number.
+	 */
+	public function getNumber() {
+		return sizeof($this->threads);
+	}
+
+	/**
+	 * @method removeTerminated
+	 * @description Removes terminated threads from the collection.
+	 * @param boolean Whether to check the threads using signal.
+	 * @return integer Number of removed threads.
+	 */
+	public function removeTerminated($check = FALSE) {
+		$n = 0;
+
+		foreach ($this->threads as $k => &$t) {
+			if ($t->terminated) {
+				unset($this->threads[$k]);
+			}
+			elseif (
+				$check 
+				&& (!$t->signal(SIGTTIN))
+			) {
+				unset($this->threads[$k]);
+			} else {
+				++$n;
+			}
+		}
+
+		return $n;
+	}
+
+	/**
+	 * @method signal
+	 * @description Sends a signal to threads.
+	 * @param integer Signal's number.
+	 * @return void
+	 */
+	public function signal($sig) {
+		foreach ($this->threads as $thread) {
+			$thread->signal($sig);
+		}
+	}
 }
