5acba13e055a069913558dd7673859feab10bad6
Kalashnikov Igor <igor.kalashnikov@gmail.com>
2010-10-03 20:38:24 +0400

    very-very slow splitting Request into Request and HTTPRequest

--- lib/Request.class.php
+++ lib/Request.class.php
@@ -36,50 +36,6 @@ class Request {
 	public $answerlen = 0;
 	public $contentLength;
 
-	public static $codes = array (
-		100 => 'Continue',
-		101 => 'Switching Protocols',
-		200 => 'OK',
-		201 => 'Created',
-		202 => 'Accepted',
-		203 => 'Non-Authoritative Information',
-		204 => 'No Content',
-		205 => 'Reset Content',
-		206 => 'Partial Content',
-		300 => 'Multiple Choices',
-		301 => 'Moved Permanently',
-		302 => 'Found',
-		303 => 'See Other',
-		304 => 'Not Modified',
-		305 => 'Use Proxy',
-		306 => '(Unused)',
-		307 => 'Temporary Redirect',
-		400 => 'Bad Request',
-		401 => 'Unauthorized',
-		402 => 'Payment Required',
-		403 => 'Forbidden',
-		404 => 'Not Found',
-		405 => 'Method Not Allowed',
-		406 => 'Not Acceptable',
-		407 => 'Proxy Authentication Required',
-		408 => 'Request Timeout',
-		409 => 'Conflict',
-		410 => 'Gone',
-		411 => 'Length Required',
-		412 => 'Precondition Failed',
-		413 => 'Request Entity Too Large',
-		414 => 'Request-URI Too Long',
-		415 => 'Unsupported Media Type',
-		416 => 'Requested Range Not Satisfiable',
-		417 => 'Expectation Failed',
-		500 => 'Internal Server Error',
-		501 => 'Not Implemented',
-		502 => 'Bad Gateway',
-		503 => 'Service Unavailable',
-		504 => 'Gateway Timeout',
-		505 => 'HTTP Version Not Supported',
-	);
-
 	/**
 	 * @method __construct
 	 * @description 
@@ -97,7 +53,10 @@ class Request {
 		$this->upstream = $upstream;
 		$this->attrs = $req->attrs;
 
-		if (isset($this->upstream->expose->value) && $this->upstream->expose->value) {
+		if (
+			isset($this->upstream->expose->value) 
+			&& $this->upstream->expose->value
+		) {
 			$this->header('X-Powered-By: phpDaemon/' . Daemon::$version);
 		}
 
@@ -131,7 +90,7 @@ class Request {
 	 * @description Called when request constructs.
 	 * @return void
 	 */
-	public function init() {}
+	protected function init() {}
 
 	/**
 	 * @method getString
@@ -153,24 +112,6 @@ class Request {
 	 * @return void
 	 */
 	public function onWrite() {}
-
-	/**
-	 * @method headers_sent
-	 * @description Analog of standard PHP function headers_sent
-	 * @return boolean Success
-	 */
-	public function headers_sent() {
-		return $this->headers_sent;
-	}
-
-	/**
-	 * @method headers_list
-	 * @description Returns current list of headers.
-	 * @return array Headers.
-	 */
-	public function headers_list() {
-		return array_values($this->headers);
-	}
 
 	/**
 	 * @method registerShutdownFunction
@@ -206,23 +147,6 @@ class Request {
 		}
 
 		return FALSE;
-	}
-
-	/**
-	 * @method status
-	 * @throws RequestHeadersAlreadySent
-	 * @param int Code
-	 * @description Sends HTTP-status (200, 403, 404, 500, etc)
-	 * @return void
-	 */
-	public function status($code = 200) {
-		if (!isset(self::$codes[$code])) {
-			return FALSE;
-		}
-
-		$this->header($code . ' ' . self::$codes[$code]);
-
-		return TRUE;
 	}
 
 	/**
@@ -350,7 +274,7 @@ class Request {
 	 * @description Called when the request wakes up.
 	 * @return void
 	 */
-	public function onWakeup() {
+	protected function onWakeup() {
 		if (!Daemon::$compatMode) {
 			Daemon::$worker->setStatus(2);
 		}
@@ -360,14 +284,6 @@ class Request {
 		$this->running = TRUE;
 
 		Daemon::$req = $this;
-
-		$_GET     = &$this->attrs->get;
-		$_POST    = &$this->attrs->post;
-		$_COOKIE  = &$this->attrs->cookie;
-		$_REQUEST = &$this->attrs->request;
-		$_SESSION = &$this->attrs->session;
-		$_FILES   = &$this->attrs->files;
-		$_SERVER  = &$this->attrs->server;
 	}
 
 	/**
@@ -384,83 +300,7 @@ class Request {
 
 		Daemon::$req = NULL;
 		$this->running = FALSE;
-	}
-
-	/**
-	 * @method setcookie
-	 * @description Sets the cookie.
-	 * @param string Name of cookie.
-	 * @param string Value.
-	 * @param integer. Optional. Max-Age. Default is 0.
-	 * @param string. Optional. Path. Default is empty string.
-	 * @param boolean. Optional. Secure. Default is false.
-	 * @param boolean. Optional. HTTPOnly. Default is false.
-	 * @return void
-	 * @throws RequestHeadersAlreadySent
-	 */
-	public function setcookie($name, $value = '', $maxage = 0, $path = '', $domain = '', $secure = FALSE, $HTTPOnly = FALSE) {
-		$this->header(
-			'Set-Cookie: ' . $name . '=' . rawurlencode($value) 
-			. (empty($domain) ? '' : '; Domain=' . $domain) 
-			. (empty($maxage) ? '' : '; Max-Age='.$maxage) 
-			. (empty($path) ? '' : '; Path='.$path) 
-			. (!$secure ? '' : '; Secure') 
-			. (!$HTTPOnly ? '' : '; HttpOnly'), false); 
-	}
-
-	/**
-	 * @method header
-	 * @description Sets the header.
-	 * @param string Header. Example: 'Location: http://php.net/'
-	 * @param boolean Optional. Replace?
-	 * @param int Optional. HTTP response code.
-	 * @return void
-	 * @throws RequestHeadersAlreadySent
-	 */
-	public function header($s, $replace = TRUE, $code = NULL) {
-		if ($code !== NULL) {
-			$this->status($code);
-		}
-
-		if ($this->headers_sent) {
-			throw new RequestHeadersAlreadySent();
-			return FALSE;
-		}
-
-		$e = explode(':', $s, 2);
-
-		if (!isset($e[1])) {
-			$e[0] = 'STATUS';
-
-			if (strncmp($s, 'HTTP/', 5) === 0) {
-				$s = substr($s, 9);
-			}
-		}
-
-		$k = strtr(strtoupper($e[0]), Request::$htr);
-
-		if (
-			!$replace 
-			&& isset($this->headers[$k])
-		) {
-			return FALSE;
-		}
-
-		$this->headers[$k] = $s;
-
-		if ($k === 'CONTENT_LENGTH') {
-			$this->contentLength = (int) $e[1];
-		}
-		elseif ($k === 'LOCATION') {
-			$this->status(301);
-		}
-
-		if (Daemon::$compatMode) {
-			is_callable('header_native') ? header_native($s) : header($s);
-		}
-
-		return TRUE;
-	}
+	}	
 
 	/**
 	 * @method parseParams
