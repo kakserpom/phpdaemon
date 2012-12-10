3bc2bcdae2c740b73627d9407255369b845e4026
kakserpom <kak.serpom.po.yaitsam@gmail.com>
2012-12-11 01:46:26 +0400

    WebSocketServer: compliance with HTTPServer

--- lib/WebSocketServerConnection.php
+++ lib/WebSocketServerConnection.php
@@ -23,7 +23,7 @@ class WebSocketServerConnection extends Connection {
 	}
 	
 	public function onInheritanceFromRequest($req) {
-		$this->stdin("\r\n" . $req->attrs->inbuf);
+		$this->stdin("\r\n" . $req->conn->bufHead);
 	}
 	
 	/**
