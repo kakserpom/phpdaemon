a5e08b5ebad0074422b4743907ec1278b274259f
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2013-05-06 21:03:03 +0400

    igbinaryFallback

--- lib/PHPDaemon/Daemon.php
+++ lib/PHPDaemon/Daemon.php
@@ -263,6 +263,17 @@ class Daemon {
 			}
 		}
 
+		if (!function_exists('igbinary_serialize')) {
+			function igbinary_serialize($m) {
+				return serialize($m);
+			}
+
+			function igbinary_unserialize($m) {
+				return unserialize($m);
+			}
+		}
+
+
 	}
 
 	/**
