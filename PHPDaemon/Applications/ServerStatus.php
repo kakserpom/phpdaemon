f52dcabdcf1516d3f6df9a10047505ec6ed05b2e
OZ <normandiggs@gmail.com>
2013-05-07 15:55:48 +0400

    Applications

--- PHPDaemon/Applications/ServerStatus.php
+++ PHPDaemon/Applications/ServerStatus.php
@@ -1,12 +1,6 @@
 <?php
 namespace PHPDaemon\Applications;
 
-/**
- * @package    Applications
- * @subpackage ServerStatus
- *
- * @author     Zorin Vasily <maintainer@daemon.io>
- */
 class ServerStatus extends \PHPDaemon\AppInstance {
 
 	/**
@@ -16,44 +10,7 @@ class ServerStatus extends \PHPDaemon\AppInstance {
 	 * @return object Request.
 	 */
 	public function beginRequest($req, $upstream) {
-		return new ServerStatus_Request($this, $upstream, $req);
-	}
-
-}
-
-class ServerStatus_Request extends HTTPRequest {
-
-	/**
-	 * Called when request iterated.
-	 * @return integer Status.
-	 */
-	public function run() {
-		$stime = microtime(TRUE);
-		$this->header('Content-Type: text/html; charset=utf-8');
-
-		?>
-		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-		<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-			<title>Server status.</title>
-		</head>
-		<body>
-		<br/>Uptime: <b><?php echo \PHPDaemon\Daemon::date_period_text(\PHPDaemon\Daemon::$startTime, time()); ?></b>
-		<br/><br/><b>State of workers:</b><?php $stat = \PHPDaemon\Daemon::getStateOfWorkers(); ?>
-		<br/>Idle: <?php echo $stat['idle']; ?>
-		<br/>Busy: <?php echo $stat['busy']; ?>
-		<br/>Total alive: <?php echo $stat['alive']; ?>
-		<br/>Shutdown: <?php echo $stat['shutdown']; ?>
-		<br/>Pre-init: <?php echo $stat['preinit']; ?>
-		<br/>Wait-init: <?php echo $stat['waitinit']; ?>
-		<br/>Init: <?php echo $stat['init']; ?>
-		<br/>
-		<br/>Request took: <?php printf('%f', round(microtime(TRUE) - $stime, 6)); ?>
-		</body>
-		</html>
-		<?php
-		return;
+		return new ServerStatusRequest($this, $upstream, $req);
 	}
 
 }
