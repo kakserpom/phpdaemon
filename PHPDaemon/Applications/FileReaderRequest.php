aa6e2deb995bd7c1bbc7dac3fc5081eb25df1cbc
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2013-05-16 20:50:30 +0400

    Merge ns

--- PHPDaemon/Applications/FileReaderRequest.php
+++ PHPDaemon/Applications/FileReaderRequest.php
@@ -1,90 +1,12 @@
 <?php
 namespace PHPDaemon\Applications;
 
-<<<<<<< HEAD:app-web/FileReader.php
-/**
- * @package Applications
- * @subpackage FileReader
- *
- * @author Zorin Vasily <maintainer@daemon.io>
- */
-class FileReader extends AppInstance {
-
-	/**
-	 * Setting default config options
-	 * Overriden from AppInstance::getConfigDefaults
-	 * @return array|false
-	 */
-	protected function getConfigDefaults() {
-		return array(
-			// index file names
-			'indexfiles'	=> 'index.html/index.htm'
-		);
-	}
-
-	/**
-	 * Constructor.
-	 * @return void
-	 */
-	public function init() {
-		$this->onConfigUpdated();
-	}
-	
-	public function onConfigUpdated() {
-		$this->indexFiles = explode('/', $this->config->indexfiles->value);
-	}
-
-	/**
-	 * Creates Request.
-	 * @param object Request.
-	 * @param object Upstream application instance.
-	 * @return object Request.
-	 */
-	public function beginRequest($req, $upstream) {
-		return new FileReaderRequest($this, $upstream, $req);
-	}
-}
-
-class FileReaderRequest extends HTTPRequest {
-=======
 use PHPDaemon\Core\Daemon;use PHPDaemon\HTTPRequest\Generic;class FileReaderRequest extends Generic {
->>>>>>> ns:PHPDaemon/Applications/FileReaderRequest.php
 
-	public $stream;
-	public $job;
-	public $indexFile;
+public $stream;
+public $job;
+public $indexFile;
 
-<<<<<<< HEAD:app-web/FileReader.php
-	/**
-	 * Constructor.
-	 * @return void
-	 */
-	public function init() {
-		if (!isset($this->attrs->server['FR_PATH'])) {
-			$this->status(404);
-			$this->finish();
-			return;
-		}
-		$job = new ComplexJob(function($job) {
-			$this->wakeup();
-		});
-		$this->job = $job;
-		$this->sleep(5, true);
-		$this->attrs->server['FR_PATH'] = FS::sanitizePath($this->attrs->server['FR_PATH']);
-		$job('stat', function($name, $job) {
-			FS::stat($this->attrs->server['FR_PATH'], function($path, $stat) use ($job) {
-				if ($stat === -1) {
-					$this->fileNotFound();
-					$job->setResult('stat', false);
-					return;
-				}
-				if ($stat['type'] === 'd') {
-					if (!FS::$supported) {
-						$this->file(rtrim($path,'/') . '/index.html');
-					}
-					else $job('readdir', function ($name, $job) use ($path) {
-						FS::readdir(rtrim($path,'/'), function ($path, $dir) use ($job) {
-=======
 /**
  * Constructor.
  * @return void
@@ -115,7 +37,6 @@ public function init() {
 				else {
 					$job('readdir', function ($name, $job) use ($path) {
 						\PHPDaemon\FS\FileSystem::readdir(rtrim($path, '/'), function ($path, $dir) use ($job) {
->>>>>>> ns:PHPDaemon/Applications/FileReaderRequest.php
 							$found = false;
 							if (is_array($dir)) {
 								foreach ($dir['dents'] as $file) {
@@ -125,35 +46,27 @@ public function init() {
 											$found = true;
 											break;
 										}
-									}									
+									}
 								}
 							}
 							if (!$found) {
 								if (isset($this->attrs->server['FR_AUTOINDEX']) && $this->attrs->server['FR_AUTOINDEX']) {
 									$this->autoindex($path, $dir);
-								} else {
+								}
+								else {
 									$this->fileNotFound();
 								}
 							}
 							$job->setResult('readdir');
-						},  EIO_READDIR_STAT_ORDER | EIO_READDIR_DENTS);
+						}, EIO_READDIR_STAT_ORDER | EIO_READDIR_DENTS);
 					});
-				} elseif ($stat['type'] == 'f') {
-					$this->file($path);
 				}
-				$job->setResult('stat', $stat);
-			});
+			}
+			elseif ($stat['type'] == 'f') {
+				$this->file($path);
+			}
+			$job->setResult('stat', $stat);
 		});
-<<<<<<< HEAD:app-web/FileReader.php
-		$job();	
-	}
-	public function fileNotFound() {
-		try {
-			$this->header('404 Not Found');
-			$this->header('Content-Type: text/html');
-		} catch (RequestHeadersAlreadySent $e ) {}
-		$this->out('File not found.');
-=======
 	});
 	$job();
 }
@@ -175,77 +88,85 @@ public function file($path) {
 	if ($warn) {
 		\PHPDaemon\Core\Daemon::log('BEWARE! You are using FileReader with EIO enabled. Maybe this is still buggy.');
 		$warn = true;
->>>>>>> ns:PHPDaemon/Applications/FileReaderRequest.php
 	}
-	public function file($path) {
-		if (!FS::$supported) {
-			$this->out(file_get_contents(realpath($path)));
-			$this->wakeup();
-			return;
-		}
-		static $warn = false;
-		if ($warn) {
-			Daemon::log('BEWARE! You are using FileReader with EIO enabled. Maybe this is still buggy.');
-			$warn = true;
-		}
-<<<<<<< HEAD:app-web/FileReader.php
-		$job = $this->job;
-		$job('readfile', function ($name, $job) use ($path) {
-			$this->sendfile($path, function($file, $success) use ($job, $name) {
-				$job->setResult($name);
-			});
+	$job = $this->job;
+	$job('readfile', function ($name, $job) use ($path) {
+		$this->sendfile($path, function ($file, $success) use ($job, $name) {
+			$job->setResult($name);
 		});
-	}
-	public function autoindex($path, $dir) {
-		$this->onWakeup();
+	});
+}
+public function autoindex($path, $dir) {
+$this->onWakeup();
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
-<head> 
-<title>Index of /</title> 
-<style type="text/css"> 
-a, a:active {text-decoration: none; color: blue;}
-a:visited {color: #48468F;}
-a:hover, a:focus {text-decoration: underline; color: red;}
-body {background-color: #F5F5F5;}
-h2 {margin-bottom: 12px;}
-table {margin-left: 12px;}
-th, td { font: 90% monospace; text-align: left;}
-th { font-weight: bold; padding-right: 14px; padding-bottom: 3px;}
-td {padding-right: 14px;}
-td.s, th.s {text-align: right;}
-div.list { background-color: white; border-top: 1px solid #646464; border-bottom: 1px solid #646464; padding-top: 10px; padding-bottom: 14px;}
-div.foot { font: 90% monospace; color: #787878; padding-top: 4px;}
-</style> 
-</head> 
-<body> 
-<pre class="header">Welcome!</pre><h2>Index of /</h2> 
-<div class="list"> 
-<table summary="Directory Listing" cellpadding="0" cellspacing="0"> 
-<thead>
-<tr>
-	<th class="n">Name</th>
-	<th class="t">Type</th>
-</tr>
-</thead> 
-<tbody> 
-<tr>
-	<td class="n"><a href="../">Parent Directory</a>/</td>
-	<td class="t">Directory</td>
-</tr> 
-<?php
-	foreach ($dir['dents'] as $item) {
-		$type = $item['type'] === EIO_DT_DIR ? 'Directory' : MIME::get($path . $item['name']);
-	?><tr>
-			<td class="n"><a href="<?php echo htmlspecialchars($item['name']) . ($type == 'Directory' ? '/' : ''); ?>"><?php echo htmlspecialchars($item['name']); ?></a></td>
-			<td class="t"><?php echo $type; ?></td>
-		</tr>
-		<?php } ?>
-</tbody> 
-</table> 
-</div> <?php if ($this->upstream->pool->config->expose->value) {?><div class="foot">phpDaemon/<?php echo Daemon::$version; ?></div><?php } ?>
-</body> 
-=======
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<title>Index of /</title>
+	<style type="text/css">
+		a, a:active
+		{
+			text-decoration: none;
+			color:           blue;
+		}
+
+		a:visited
+		{
+			color: #48468F;
+		}
+
+		a:hover, a:focus
+		{
+			text-decoration: underline;
+			color:           red;
+		}
+
+		body
+		{
+			background-color: #F5F5F5;
+		}
+
+		h2
+		{
+			margin-bottom: 12px;
+		}
+
+		table
+		{
+			margin-left: 12px;
+		}
+
+		th, td
+		{
+			font:       90% monospace;
+			text-align: left;
+		}
+
+		th
+		{
+			font-weight:    bold;
+			padding-right:  14px;
+			padding-bottom: 3px;
+		}
+
+		td
+		{
+			padding-right: 14px;
+		}
+
+		td.s, th.s
+		{
+			text-align: right;
+		}
+
+		div.list
+		{
+			background-color: white;
+			border-top:       1px solid #646464;
+			border-bottom:    1px solid #646464;
+			padding-top:      10px;
+			padding-bottom:   14px;
+		}
 
 		div.foot
 		{
@@ -289,20 +210,19 @@ div.foot { font: 90% monospace; color: #787878; padding-top: 4px;}
 	?>
 	<div class="foot">phpDaemon/<?php echo Daemon::$version; ?></div><?php } ?>
 </body>
->>>>>>> ns:PHPDaemon/Applications/FileReaderRequest.php
 </html><?php
-	}
-	/**
-	 * Called when the request aborted.
-	 * @return void
-	 */
-	public function onAbort() {
-		$this->finish();
-	}
+}
+/**
+ * Called when the request aborted.
+ * @return void
+ */
+public function onAbort() {
+	$this->finish();
+}
 
-	/**
-	 * Called when request iterated.
-	 * @return integer Status.
-	 */
-	public function run() {}	
+/**
+ * Called when request iterated.
+ * @return integer Status.
+ */
+public function run() { }
 }
--- PHPDaemon/Applications/ServerStatusRequest.php
+++ PHPDaemon/Applications/ServerStatusRequest.php
@@ -11,25 +11,7 @@ use PHPDaemon\HTTPRequest\Generic;
  *
  * @author Zorin Vasily <maintainer@daemon.io>
  */
-<<<<<<< HEAD:app-web/ServerStatus.php
-class ServerStatus extends AppInstance {
-
-	/**
-	 * Creates Request.
-	 * @param object Request.
-	 * @param object Upstream application instance.
-	 * @return object Request.
-	 */
-	public function beginRequest($req, $upstream) {
-		return new ServerStatus_Request($this, $upstream, $req);
-	}
-	
-}
-
-class ServerStatus_Request extends HTTPRequest {
-=======
 class ServerStatusRequest extends Generic {
->>>>>>> ns:PHPDaemon/Applications/ServerStatusRequest.php
 
 	/**
 	 * Called when request iterated.
@@ -38,9 +20,8 @@ class ServerStatusRequest extends Generic {
 	public function run() {
 		$stime = microtime(TRUE);
 		$this->header('Content-Type: text/html; charset=utf-8');
-
 		?>
-<<<<<<< HEAD:app-web/ServerStatus.php
+
  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml"> 
 <head> 
@@ -61,31 +42,6 @@ class ServerStatusRequest extends Generic {
 <br />Request took: <?php printf('%f', round(microtime(TRUE) - $stime, 6)); ?>
 </body>
 </html>
-<?php
-=======
-		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-		<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-			<title>Server status.</title>
-		</head>
-		<body>
-		<br/>Uptime: <b><?php echo \PHPDaemon\Core\Daemon::date_period_text(\PHPDaemon\Core\Daemon::$startTime, time()); ?></b>
-		<br/><br/><b>State of workers:</b><?php $stat = \PHPDaemon\Core\Daemon::getStateOfWorkers(); ?>
-		<br/>Idle: <?php echo $stat['idle']; ?>
-		<br/>Busy: <?php echo $stat['busy']; ?>
-		<br/>Total alive: <?php echo $stat['alive']; ?>
-		<br/>Shutdown: <?php echo $stat['shutdown']; ?>
-		<br/>Pre-init: <?php echo $stat['preinit']; ?>
-		<br/>Wait-init: <?php echo $stat['waitinit']; ?>
-		<br/>Init: <?php echo $stat['init']; ?>
-		<br/>
-		<br/>Request took: <?php printf('%f', round(microtime(TRUE) - $stime, 6)); ?>
-		</body>
-		</html>
-		<?php
->>>>>>> ns:PHPDaemon/Applications/ServerStatusRequest.php
-		return;
 	}
 	
 }
