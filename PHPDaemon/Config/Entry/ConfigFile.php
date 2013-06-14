abc6375f8357ec96ff4a0c0b52b589f8b669fb32
OZ <normandiggs@gmail.com>
2013-06-14 14:39:28 +0400

    small fix in ConfigFile

--- PHPDaemon/Config/Entry/ConfigFile.php
+++ PHPDaemon/Config/Entry/ConfigFile.php
@@ -22,7 +22,7 @@ class ConfigFile extends Generic {
 
 		$e = explode(';', $old);
 		foreach ($e as $path) {
-			Daemon::$process->fileWatcher->rmWatch($path);
+			Daemon::$process->fileWatcher->rmWatch($path, [Daemon::$process, 'sighup']);
 		}
 
 		$e = explode(';', $this->value);
