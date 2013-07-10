75f0c9955dcd00da7e85ab9e500cbd536baa5fa4
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2013-07-10 15:31:53 +0400

    Gibson client dev

--- PHPDaemon/Clients/Gibson/Pool.php
+++ PHPDaemon/Clients/Gibson/Pool.php
@@ -7,28 +7,17 @@ use PHPDaemon\Core\Daemon;
 class Pool extends \PHPDaemon\Network\Client {
 
 	protected  $opCodes = [
-		'set' => 1,
-		'ttl' => 2,
-		'get' => 3,
-		'del' => 4,
-		'inc' => 5,
-		'dec' => 6,
-		'lock' => 7,
-		'unlock' => 8,
-		'mset' => 9,
-		'mttl' => 10,
-		'mget' => 11,
-		'mdel' => 12,
-		'minc' => 13,
-		'mdec' => 14,
-		'mlock' => 15,
-		'munlock' => 16,
-		'count' => 17,
-		'stats' => 18,
-		'ping' => 19,
-		'sizeof' => 20,
-		'msizeof' => 21,
-		'encof' => 22,
+		'set' => 1,		'ttl' => 2,
+		'get' => 3,		'del' => 4,
+		'inc' => 5,		'dec' => 6,
+		'lock' => 7,	'unlock' => 8,
+		'mset' => 9,	'mttl' => 10,
+		'mget' => 11,	'mdel' => 12,
+		'minc' => 13,	'mdec' => 14,
+		'mlock' => 15,	'munlock' => 16,
+		'count' => 17,	'stats' => 18,
+		'ping' => 19,	'sizeof' => 20,
+		'msizeof' => 21,'encof' => 22,
 		'end' => 255,
 	];
 
@@ -65,14 +54,11 @@ class Pool extends \PHPDaemon\Network\Client {
 		if (!isset($this->opcodes[$name])) {
 			return false;
 		}
-		$code = $this->opcodes[$name];
 		$onResponse = null;
 		if (($e = end($args)) && (is_array($e) || is_object($e)) && is_callable($e)) {
 			$onResponse = array_pop($args);
 		}
 		$data = implode(' ', $args);
-		$qLen = strlen($data) + 2;
-		$r = pack('ls', $qLen, $opcode) . $data;
-		$this->requestByServer($server = null, $r, $onResponse);
+		$this->requestByServer($server = null, pack('ls', strlen($data) + 2, $this->opcodes[$name]) . $data, $onResponse);
 	}
 }
