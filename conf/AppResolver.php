e175284c0ffb5d31ae2257f15f81b46efe83d3fc
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2013-07-10 15:26:31 +0400

    conf/AppResolver.php restored

--- conf/AppResolver.php
+++ conf/AppResolver.php
@@ -6,12 +6,7 @@
  * @package Core
  * @author  Zorin Vasily <maintainer@daemon.io>
  */
-
-
-use PHPDaemon\Core\Daemon;
-
-class MyAppResolver extends \PHPDaemon\Core\AppResolver{
-
+class MyAppResolver extends \PHPDaemon\Core\AppResolver {
 
     /**
      * Routes incoming request to related application. Method is for overloading.
@@ -19,18 +14,15 @@ class MyAppResolver extends \PHPDaemon\Core\AppResolver{
      * @param object AppInstance of Upstream.
      * @return string Application's name.
      */
-    public function getRequestRoute($req, $upstream){
+    public function getRequestRoute($req, $upstream) {
 
         /*
             This method should return application name to handle incoming request ($req).
         */
 
-        if(strpos($req->attrs->server['DOCUMENT_URI'], 'GibsonTest'))
-            return 'GibsonTest';
-
-        /*  $route = pathinfo($req->attrs->server['DOCUMENT_URI']);
-          return $route['filename'];*/
-
+        if (preg_match('~^/(WebSocketOverCOMET|Example.*)/?~', $req->attrs->server['DOCUMENT_URI'], $m)) {
+            return $m[1];
+        }
 
         /* Example
         $host = basename($req->attrs->server['HTTP_HOST']);
