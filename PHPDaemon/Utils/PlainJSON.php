71b046eb860ea29dd1bdd8cd8d59801628e68bf8
EzheG <edg89@mail.ru>
2015-02-26 16:03:22 +0300

    add Utils\PlainJSON

---
+++ PHPDaemon/Utils/PlainJSON.php
@@ -0,0 +1,57 @@
+<?php
+namespace PHPDaemon\Utils;
+use PHPDaemon\Core\Daemon;
+
+/**
+ * PlainJSON
+ * @package   PHPDaemon\Utils
+ * @author    Zorin Vasily <maintainer@daemon.io>
+ * @author    Efimenko Dmitriy <ezheg89@gmail.com>
+ *
+ * Use:
+ *  $obj1 = new PlainJSON('{"name":"John"}');
+ *  $obj2 = new PlainJSON('{"name":"Smit"}');
+ *  echo PlainJSON::apply(json_encode([ ['name' => 'Mary' ], $obj1, $obj2]));
+ *  // [{"name":"Mary"},{"name":"John"},{"name":"Smit"}]
+ */
+class PlainJSON implements \JsonSerializable {
+	use \PHPDaemon\Traits\ClassWatchdog;
+	use \PHPDaemon\Traits\StaticObjectWatchdog;
+
+	protected $id;
+	
+	static protected $tr = [];
+
+	/**
+	 * Save
+	 * @param string $str JSON string
+	 */
+	public function __construct($str) {
+		$this->id = Daemon::uniqid();
+		static::$tr['"' . $this->id . '"'] = $str;
+	}
+
+	/**
+	 * Clean cache
+	 */
+	public function __destruct() {
+		unset(static::$tr[$this->id]);
+	}
+
+	/**
+	 * jsonSerialize
+	 * @return string
+	 */
+	public function jsonSerialize() {
+		return $this->id;
+	}
+
+	/**
+	 * Apply
+	 * @param  string $data JSON string
+	 * @return string       Result JSON
+	 */
+	public static function apply($data) {
+		return strtr($data, static::$tr);
+	}
+}
