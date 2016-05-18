8ac233cf5d597a331b78e28c9b69eeb21a4cd525
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2016-05-19 06:58:22 +0700

    * [Test] Utils\Crypt

---
+++ tests/Generic.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace Tests;
+
+use PHPDaemon\Core\EventLoop;
+use PHPDaemon\Core\Timer;
+
+abstract class Generic extends \PHPUnit_Framework_TestCase
+{
+    protected function prepareAsync()
+    {
+        EventLoop::init();
+    }
+
+    protected function completeAsync() {
+        EventLoop::$instance->stop();
+    }
+    protected function runAsync($method, $timeout = 3e6)
+    {
+        Timer::add(function () use ($method) {
+            self::assertSame(0, 1, 'Some callbacks didnt finished in ' . $method);
+        }, $timeout);
+        $this->loop->run();
+        EventLoop::$instance->free();
+        EventLoop::$instance = null;
+    }
+}
---
+++ tests/Utils/CryptTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Patsura Dmitry https://github.com/ovr <talk@dmtry.me>
+ */
+
+namespace Tests\Utils;
+
+use PHPDaemon\Utils\Crypt;
+use Tests\Generic;
+
+class CryptTest extends Generic
+{
+    public function testRandomInts()
+    {
+        $this->prepareAsync();
+
+        Crypt::randomInts(5, function ($ints) {
+            self::assertCount(5, $ints, '$ints must contain 5 elements');
+            $this->completeAsync();
+        });
+
+        $this->runAsync(__METHOD__);
+    }
+}
