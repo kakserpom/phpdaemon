db0903888cea5732a23743057f9e9eb148dc024b
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2016-05-19 07:20:50 +0700

    More cowbell

--- tests/Utils/CryptTest.php
+++ tests/Utils/CryptTest.php
@@ -5,20 +5,28 @@
 
 namespace Tests\Utils;
 
+use PHPDaemon\Core\ComplexJob;
 use PHPDaemon\Utils\Crypt;
-use Tests\Generic;
+use Tests\AbstractTestCase;
 
-class CryptTest extends Generic
+class CryptTest extends AbstractTestCase
 {
     public function testRandomInts()
     {
         $this->prepareAsync();
 
-        Crypt::randomInts(5, function ($ints) {
-            self::assertCount(5, $ints, '$ints must contain 5 elements');
+        $cj = new ComplexJob(function($cj) {
             $this->completeAsync();
         });
-
+        for ($i = 1; $i < 25; ++$i) {
+            $cj($i, function($i, $cj) {
+                Crypt::randomInts($i, function ($ints) use ($i, $cj) {
+                    self::assertCount($i, $ints, '$ints must contain ' . $i . ' element(s)');
+                    $cj[$i] = true;
+                });
+            });
+        }
+        $cj();
         $this->runAsync(__METHOD__);
     }
 }
